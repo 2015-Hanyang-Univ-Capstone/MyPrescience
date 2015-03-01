87fd6f9fc6b75da2e9f7007148bc24294ddb8b72
Hyeon-Seob Seo <hsseo417@gmail.com>
2015-03-01 21:18:50 +0900

    Initial commit

---
+++ app/src/main/java/com/myprescience/application/myprescience/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.myprescience.application.myprescience;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.facebook.AppEventsLogger;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+}
