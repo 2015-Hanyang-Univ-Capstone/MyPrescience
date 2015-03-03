f2eb09e4e421c50e3c4da47afe770aeb8c94ba41
hyeon-seob <hsseo417@gmail.com>
2015-03-03 17:51:04 +0900

    장르 선택 까지

--- app/src/main/java/com/myprescience/application/myprescience/LoginActivity.java
+++ app/src/main/java/com/myprescience/application/myprescience/LoginActivity.java
@@ -1,17 +1,58 @@
 package com.myprescience.application.myprescience;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.facebook.AppEventsLogger;
+import com.facebook.widget.LoginButton;
+
+import java.util.Arrays;
 
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends FragmentActivity {
+    private MainFragment mainFragment;
+    private Button guestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        if (savedInstanceState == null) {
+//            // Add the fragment on initial activity setup
+//            mainFragment = new MainFragment();
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .add(android.R.id.content, mainFragment)
+//                    .commit();
+//        } else {
+//            // Or set the fragment from restored state info
+//            mainFragment = (MainFragment) getSupportFragmentManager()
+//                    .findFragmentById(android.R.id.content);
+//        }
+
         setContentView(R.layout.activity_login);
+
+        LoginButton authButton = (LoginButton) findViewById(R.id.authButton);
+        authButton.setReadPermissions(Arrays.asList("public_profile"));
+
+        // 임시용 게스트 로그인 버튼
+        guestButton = (Button) findViewById(R.id.guestButton);
+        guestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(LoginActivity.this, "게스트로 시작합니다.", Toast.LENGTH_SHORT);
+                toast.show();
+
+                Intent intent = new Intent(LoginActivity.this, RecommendActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 
     @Override
